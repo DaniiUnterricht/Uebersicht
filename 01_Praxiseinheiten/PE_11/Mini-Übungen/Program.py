821eaa3d0538ae3c3b78647af35bfb07118f5e54
Danii <danii.wittmeier@gmail.com>
2026-04-26 16:51:01 +0200

    Miniübungen PE 11 erledigt

--- 01_Praxiseinheiten/PE_11/Mini-Übungen/Program.py
+++ 01_Praxiseinheiten/PE_11/Mini-Übungen/Program.py
@@ -1,8 +1,34 @@
 # Übung 01
 ## Machen Sie eine Abfrage, welche eine Zahl abfragt und bei falscher Eingabe wiederholt
+while True:
+    try:
+        inp = int(input("Bitte geben Sie eine Zahl ein: "))
+    except:
+        print("Ungültige Eingabe.")
+    else:
+        break
 
+print()
 # Übung 02
 ## Summieren Sie solange die Zahlen, bis 0 eingegeben wird
+sum = 0
+while True:
+    try:
+        inp = int(input("Bitte geben Sie eine Zahl ein: "))
+    except:
+        print("Ungültige Eingabe.")
+    else:
+        if inp != 0:
+            sum += inp
+        else:
+            break
+print(sum)
 
+print()
 # Übung 03
 ## Erstellen Sie eine einfache Multiplikationstabelle
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        print(f"{i} x {j} = {i * j}")
+    print()
