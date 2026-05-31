816b94cc7fc72a1ae4b95c1e594da36a710ed0d7
Danii <danii.wittmeier@gmail.com>
2026-05-31 17:37:40 +0200

    PE14 Miniübung erledigt.

--- 01_Praxiseinheiten/PE_14/Uebung/Program.cs
+++ 01_Praxiseinheiten/PE_14/Uebung/Program.cs
@@ -43,22 +43,113 @@ int Menue()
 
 void ShowPlayer(Dictionary<string, int> player)
 {
-
+    if (player.Count > 0)
+    {
+	//Man könnte hier auch: "foreach (var kvp in player)" schreiben
+        foreach (KeyValuePair<string, int> kvp in player)
+        {
+            Console.WriteLine($"{kvp.Key} hat {kvp.Value} Punkte.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Keine Spieler in der Dictionary vorhanden.");
+    }
 }
 
 void NewPlayer(Dictionary<string, int> player)
 {
+    bool isTrue;
+    int points;
+    string playername;
 
+    do
+    {
+        Console.Write("Bitte Name des Spielers eingeben: ");
+        playername = Console.ReadLine()!;
+        if (playername == "")
+        {
+            Console.WriteLine("Der Name darf nicht Leer sein!");
+            isTrue = true;
+        }
+        else if (player.ContainsKey(playername))
+        {
+            Console.WriteLine("Dieser Spieler exisitiert bereits!");
+            isTrue = true;
+        }
+        else
+        {
+            isTrue = false;
+        }
+
+    } while (isTrue);
+
+    points = InputPoints();
+
+    player.Add(playername, points);
 }
 
 void EditPlayer(Dictionary<string, int> player)
 {
+    int points;
+    string playername;
 
+    playername = ContainsPlayer(player);
+
+    points = InputPoints();
+
+    player[playername] = points;
 }
 
 void DeletePlayer(Dictionary<string, int> player)
 {
+    string playername;
 
+    playername = ContainsPlayer(player);
+
+    player.Remove(playername);
+}
+
+int InputPoints()
+{
+    bool isTrue;
+    int points;
+
+    do
+    {
+        Console.Write("Bitte geben Sie die Punkte des Spielers ein: ");
+        isTrue = int.TryParse(Console.ReadLine(), out points);
+
+        if (!isTrue)
+        {
+            Console.WriteLine("Ungültige Eingabe!");
+        }
+    } while (!isTrue);
+
+    return points;
+}
+
+string ContainsPlayer(Dictionary<string,int> player)
+{
+    string playername;
+    bool isTrue;
+
+    do
+    {
+        Console.Write("Bitte Name des Spielers eingeben: ");
+        playername = Console.ReadLine()!;
+        if (!player.ContainsKey(playername))
+        {
+            Console.WriteLine("Dieser Spieler existiert nicht!");
+            isTrue = true;
+        }
+        else
+        {
+            isTrue = false;
+        }
+    } while (isTrue);
+
+    return playername;
 }
 
 int auswahl;
